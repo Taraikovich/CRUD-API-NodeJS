2f6b1858b5093907d0df615684328a16db371051
Taraikovich <taraikovi4@gmail.com>
2024-10-21 19:56:51 +0400

    feat: add tests

--- src/DB/usersDB.ts
+++ src/DB/usersDB.ts
@@ -5,17 +5,4 @@ export type User = {
   hobbies: string[] | [];
 };
 
-export const usersDB: User[] = [
-  {
-    id: 'b3f71f90-2921-42fe-941b-7484b2aa7b07',
-    username: 'Igor',
-    age: 38,
-    hobbies: ['soccer', 'tennis', 'programing'],
-  },
-  {
-    id: 'b3f71f90-2921-42fe-941b-7484b2aa7b05',
-    username: 'Avtandil',
-    age: 32,
-    hobbies: ['soccer', 'haircut'],
-  },
-];
+export const usersDB: User[] = [];
